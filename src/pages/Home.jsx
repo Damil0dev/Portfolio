208eb3f2e338aaafc984406a2974e048fc75ec9f
Damil0dev <ysladamian@outlook.com>
2023-09-25 13:30:48 -0300

    made Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div>
+        <div className="w-full h-screen px-64 flex justify-center items-center">
             <Welcome />
         </div>
     );
@@ -8,9 +8,9 @@ export default function Home() {
 
 function Welcome() {
     return (
-        <div>
-            <h1>Damian Ysla</h1>
-            <p>I'm a <h2>Front End Developer</h2></p>
+        <div className="flex flex-col justify-center items-center gap-5 text-center">
+            <h1 className="font-bold text-6xl border-b-4 border-blue-800" >Damian Ysla</h1>
+            <p className="text-3xl text-center">I&apos;m a <h2 className="inline font-bold text-blue-800">Front End Developer</h2> and <h2 className="inline font-bold text-blue-800">Entrepreneur</h2> from Argentina</p>
         </div>
 
     )
