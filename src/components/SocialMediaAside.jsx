61e0589bdfc446980abf57cd3d2a7698303b131d
Damil0dev <ysladamian@outlook.com>
2023-09-26 17:35:26 -0300

    add urls to my social networks

--- src/components/SocialMediaAside.jsx
+++ src/components/SocialMediaAside.jsx
@@ -7,19 +7,19 @@ export function SocialMediaAside() {
     return (
         <aside>
             <ul className="flex flex-col gap-5">
-                <SocialNetwork imageUrl={linkedinIcon} alt="Damian's Linkedin" />
-                <SocialNetwork imageUrl={githubIcon} alt="Damian's github" /> 
-                <SocialNetwork imageUrl={whatsappIcon} alt="Damian's whatsapp" /> 
-                <SocialNetwork imageUrl={instagramIcon} alt="Damian's instagram" /> 
+                <SocialNetwork imageUrl={linkedinIcon} alt="Damian's Linkedin" link="https://www.linkedin.com/in/damian-ysla-a31054267/" />
+                <SocialNetwork imageUrl={githubIcon} alt="Damian's github" link="https://github.com/Damil0dev" /> 
+                <SocialNetwork imageUrl={whatsappIcon} alt="Damian's whatsapp" link="https://wa.me/543456467547" /> 
+                <SocialNetwork imageUrl={instagramIcon} alt="Damian's instagram" link="https://instagram.com/chino_ysla?igshid=OGQ5ZDc2ODk2ZA==" /> 
             </ul>
         </aside>
     )
 }
 
-function SocialNetwork({imageUrl, alt}) {
+function SocialNetwork({imageUrl, alt, link}) {
     return (
         <li className='flex '>
-            <a href="" className='p-5 hover:scale-110 transition-transform'>
+            <a href={link} target='_blank' className='p-5 hover:scale-110 transition-transform' rel="noreferrer">
                 <img src={imageUrl} alt={alt} height={32} width={32} />
             </a>
         </li>
