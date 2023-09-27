a8af3db076c20a757a207f4a1ec9e71675b8e084
Damil0dev <ysladamian@outlook.com>
2023-09-27 19:26:43 -0300

    fix a nesting issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Welcome() {
     return (
         <div className="flex flex-col justify-center items-center gap-5 text-center">
             <h1 className="font-bold text-4xl md:text-6xl border-b-4 border-blue-800" >Damian Ysla</h1>
-            <p className="text-2xl md:text-3xl text-center">I&apos;m a <h2 className="inline font-bold text-blue-800">Front End Developer</h2> and <h2 className="inline font-bold text-blue-800">Entrepreneur</h2> from Argentina</p>
+            <p className="text-2xl md:text-3xl text-center">I&apos;m a <span className="inline font-bold text-blue-800">Front End Developer</span> and <span className="inline font-bold text-blue-800">Entrepreneur</span> from Argentina</p>
         </div>
 
     )
