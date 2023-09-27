af7af9301dcc152abed244fed41902c4956957cf
Damil0dev <ysladamian@outlook.com>
2023-09-27 11:15:06 -0300

    adjust responsive design in home pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 function About() {
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center gap-5 px-32 md:px-64">
-            <Title />
-            <Text />
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <div className="max-w-xl flex flex-col gap-5 px-5">
+                <Title />
+                <Text />
+            </div>
         </div>
     );
 }
@@ -10,14 +12,14 @@ function About() {
 function Title() {
     return (
         <div>
-            <h2 className="text-center text-5xl font-bold"><span className="text-blue-800">Tech Enthusiast</span> & Aspiring <span className="text-blue-800">Developer</span></h2>
+            <h2 className="md:text-center text-4xl md:text-5xl font-bold"><span className="text-blue-800">Tech Enthusiast</span> & Aspiring <span className="text-blue-800">Developer</span></h2>
         </div>
     )
 }
 
 function Text() {
     return (
-        <div className="text-center text-2xl">
+        <div className="md:text-center text-2xl md:text-2xl">
             <p>I am Damián, a technology and programming enthusiast born in Argentina. I master technologies such as JS, React, among others. I have a PC repair business and I aspire to be a great programmer, always looking to learn and improve in any area.</p>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 export default function Home() {
     return (
-        <div className="w-full h-screen px-64 flex justify-center items-center">
-            <Welcome />
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="max-w-xl flex flex-col gap-5 px-5">
+                <Welcome />
+            </div>
+
         </div>
     );
 }
@@ -9,8 +12,8 @@ export default function Home() {
 function Welcome() {
     return (
         <div className="flex flex-col justify-center items-center gap-5 text-center">
-            <h1 className="font-bold text-6xl border-b-4 border-blue-800" >Damian Ysla</h1>
-            <p className="text-3xl text-center">I&apos;m a <h2 className="inline font-bold text-blue-800">Front End Developer</h2> and <h2 className="inline font-bold text-blue-800">Entrepreneur</h2> from Argentina</p>
+            <h1 className="font-bold text-4xl md:text-6xl border-b-4 border-blue-800" >Damian Ysla</h1>
+            <p className="text-2xl md:text-3xl text-center">I&apos;m a <h2 className="inline font-bold text-blue-800">Front End Developer</h2> and <h2 className="inline font-bold text-blue-800">Entrepreneur</h2> from Argentina</p>
         </div>
 
     )
