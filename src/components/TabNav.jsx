0f5158c70e1b492180feb1efa99c6962414770b1
Damil0dev <ysladamian@outlook.com>
2023-09-27 13:31:43 -0300

    fix two bug with the menu button in mobile

--- src/components/TabNav.jsx
+++ src/components/TabNav.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link, useLocation } from "react-router-dom";
 
 import menuIcon from "../assets/menu.svg"
 
 export function TabNav() {
     const [isOpen, setIsOpen] = useState(false);
+    const navRef = useRef(null);
 
-    function handleClick() {
-        setIsOpen(!isOpen)
-    }
+    useEffect(() => {
+        function handleDocumentClick(event) {
+            if (navRef.current && !navRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+
+        if (isOpen) {
+            document.addEventListener('mousedown', handleDocumentClick);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleDocumentClick);
+        };
+    }, [isOpen]);
 
     return (
-        <nav className={` w-screen md:flex md:flex-row md:justify-center md:items-center ${isOpen ? "bg-slate-100 shadow-lg" : "bg-transparent"} md:bg-transparent md:shadow-none`}>
-            <div className={` md:self-start md:justify-self-start md:hidden`} onClick={handleClick}>
+        <nav ref={navRef} className={`w-screen md:flex md:flex-row md:justify-center md:items-center ${isOpen ? "bg-slate-100 shadow-lg" : "bg-transparent"} md:bg-transparent md:shadow-none`}>
+            <div className={`md:self-start md:justify-self-start md:hidden`} onClick={() => { setIsOpen(!isOpen) }}>
                 <img src={menuIcon} alt="menu button" height={70} width={70} />
             </div>
 
@@ -26,6 +39,7 @@ export function TabNav() {
         </nav>
     )
 }
+
 
 function Tab({ title, path }) {
     const location = useLocation();
