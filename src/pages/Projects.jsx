3d7dfa511e245a5967e01fd8e54b58a9abca0769
Damil0dev <ysladamian@outlook.com>
2023-09-28 16:42:31 -0300

    remove an unused class

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,7 @@ import pomodoroIcon from "../assets/pomodorotimer-icon.png"
 function Projects() {
     return (
         <div className="w-full h-screen flex justify-center items-center md:py-20 px-4">
-            <div className="w-full md:w-5/6 h-3/4 md:h-fit md:h-full flex flex-col md:flex-row md:flex-wrap md:justify-center md:py-20 gap-5 overflow-auto ">
+            <div className="w-full md:w-5/6 h-3/4 md:h-fit flex flex-col md:flex-row md:flex-wrap md:justify-center md:py-20 gap-5 overflow-auto ">
                 <Project image={multiConvertIcon} alt="MultiConvert icon" title="MultiConvert" description="A converter that displays multiple conversions at once." typeOfProject="PWA" url="https://multi-convert-by-damian-ysla.web.app/"/>
                 <Project image={rickandmortyIcon} alt="Rick and Morty project icon" title="Rick and Morty pokedex" description="Practice made with the Rick and Morty API." typeOfProject="Website" url="https://rick-and-morty-pokedex.web.app/" />
                 <Project image={nasaGalleryIcon} alt="Nasa Gallery icon" title="Nasa Gallery" description="Mobile app made with the NASA API, made with React Native." typeOfProject="Mobile App" url="https://www.mediafire.com/file/g3j28wj93gzjlke/application-15e28c00-48f9-4bdc-8b91-61ee680230e3.apk/file" />
