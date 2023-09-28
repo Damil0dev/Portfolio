7adfc686d106ff93026fa3a2c8df76f29e3a411b
Damil0dev <ysladamian@outlook.com>
2023-09-28 15:33:05 -0300

    add links to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,19 +8,19 @@ function Projects() {
     return (
         <div className="w-full h-screen flex justify-center items-center md:py-24 px-4">
             <div className="w-full md:w-5/6 h-3/4 md:h-full flex flex-col md:flex-row md:flex-wrap md:justify-center gap-5 overflow-auto md:overflow-hidden">
-                <Project image={multiConvertIcon} alt="MultiConvert icon" title="MultiConvert" description="A converter that displays multiple conversions at once." typeOfProject="PWA" />
-                <Project image={rickandmortyIcon} alt="Rick and Morty project icon" title="Rick and Morty pokedex" description="Practice made with the Rick and Morty API." typeOfProject="Website" />
-                <Project image={nasaGalleryIcon} alt="Nasa Gallery icon" title="Nasa Gallery" description="Mobile app made with the NASA API, made with React Native." typeOfProject="Mobile App" />
-                <Project image={pomodoroIcon} alt="Pomodoro Timer icon" title="Pomodoro Timer" description="Mobile app that displays a pomodoro timer with 3 modes, made with React Native." typeOfProject="Mobile App" />
-                <Project image={RPSIcon} alt="Rock, paper and scissors icon" title="Rock, paper and scissors game" description="A simple game made with React." typeOfProject="Website" />
+                <Project image={multiConvertIcon} alt="MultiConvert icon" title="MultiConvert" description="A converter that displays multiple conversions at once." typeOfProject="PWA" url="https://multi-convert-by-damian-ysla.web.app/"/>
+                <Project image={rickandmortyIcon} alt="Rick and Morty project icon" title="Rick and Morty pokedex" description="Practice made with the Rick and Morty API." typeOfProject="Website" url="https://rick-and-morty-pokedex.web.app/" />
+                <Project image={nasaGalleryIcon} alt="Nasa Gallery icon" title="Nasa Gallery" description="Mobile app made with the NASA API, made with React Native." typeOfProject="Mobile App" url="https://www.mediafire.com/file/g3j28wj93gzjlke/application-15e28c00-48f9-4bdc-8b91-61ee680230e3.apk/file" />
+                <Project image={pomodoroIcon} alt="Pomodoro Timer icon" title="Pomodoro Timer" description="Mobile app that displays a pomodoro timer with 3 modes, made with React Native." typeOfProject="Mobile App" url="https://www.mediafire.com/file/48h2qpjreiqrh73/application-e848ddfb-a259-455c-bc6c-2965429b3186.apk/file" />
+                <Project image={RPSIcon} alt="Rock, paper and scissors icon" title="Rock, paper and scissors game" description="A simple game made with React." typeOfProject="Website" url="https://rock-paper-scissors-39428.web.app/" />
             </div>
         </div>
     );
 }
 
-function Project({image, alt, title, description, typeOfProject}) {
+function Project({image, alt, title, description, typeOfProject, url}) {
     return (
-        <div className="w-full md:w-1/4 flex flex-col rounded-md border-2 border-gray-200 shadow-md">
+        <div className="w-full md:w-1/4 flex flex-col rounded-md border-2 border-gray-200 shadow-md" onClick={() => window.open(url, "_blank")}>
             <div className="w-full h-2/4 flex justify-center items-center border-b-2 rounded-t-md">
                 <img src={image} alt={alt} className="my-3" width={100} height={100} />
             </div>
