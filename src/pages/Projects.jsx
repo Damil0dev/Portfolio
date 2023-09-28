19b60d82a9ac36dd66346705e6f4812e4560b75d
Damil0dev <ysladamian@outlook.com>
2023-09-28 16:36:46 -0300

    fix some details in projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,8 +6,8 @@ import pomodoroIcon from "../assets/pomodorotimer-icon.png"
 
 function Projects() {
     return (
-        <div className="w-full h-screen flex justify-center items-center md:py-24 px-4">
-            <div className="w-full md:w-5/6 h-3/4 md:h-full flex flex-col md:flex-row md:flex-wrap md:justify-center gap-5 overflow-auto md:overflow-hidden">
+        <div className="w-full h-screen flex justify-center items-center md:py-20 px-4">
+            <div className="w-full md:w-5/6 h-3/4 md:h-fit md:h-full flex flex-col md:flex-row md:flex-wrap md:justify-center md:py-20 gap-5 overflow-auto ">
                 <Project image={multiConvertIcon} alt="MultiConvert icon" title="MultiConvert" description="A converter that displays multiple conversions at once." typeOfProject="PWA" url="https://multi-convert-by-damian-ysla.web.app/"/>
                 <Project image={rickandmortyIcon} alt="Rick and Morty project icon" title="Rick and Morty pokedex" description="Practice made with the Rick and Morty API." typeOfProject="Website" url="https://rick-and-morty-pokedex.web.app/" />
                 <Project image={nasaGalleryIcon} alt="Nasa Gallery icon" title="Nasa Gallery" description="Mobile app made with the NASA API, made with React Native." typeOfProject="Mobile App" url="https://www.mediafire.com/file/g3j28wj93gzjlke/application-15e28c00-48f9-4bdc-8b91-61ee680230e3.apk/file" />
@@ -20,7 +20,7 @@ function Projects() {
 
 function Project({image, alt, title, description, typeOfProject, url}) {
     return (
-        <div className="w-full md:w-1/4 flex flex-col rounded-md border-2 border-gray-200 shadow-md" onClick={() => window.open(url, "_blank")}>
+        <div className="w-full md:w-1/4 flex flex-col rounded-md border-2 border-gray-200 shadow-md cursor-pointer hover:shadow-2xl transition-shadow" onClick={() => window.open(url, "_blank")}>
             <div className="w-full h-2/4 flex justify-center items-center border-b-2 rounded-t-md">
                 <img src={image} alt={alt} className="my-3" width={100} height={100} />
             </div>
