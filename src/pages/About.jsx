056c95c17f0704f2cd88c9ea11e56f22d8d5c7ce
Damian Ysla <ysladamian@outlook.com>
2023-10-06 17:55:26 -0300

    tweak some details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,8 +9,10 @@ function About() {
         <Title />
         <Text />
       </div>
-      <div>
-        <h2 className="p-3 text-4xl text-neutral-800 text-center font-semibold">Certificates</h2>
+      <div className='flex flex-col items-center'>
+        <h2 className='w-fit m-2 text-4xl text-neutral-800 dark:text-neutral-200 border-b-4 border-blue-800 text-center font-semibold'>
+          Certificates
+        </h2>
         <Certificados />
       </div>
     </div>
@@ -65,9 +67,9 @@ function Certificados() {
 
 function Certificado({ src, alt, title }) {
   return (
-    <div className='md:w-1/5 bg-blue-800 rounded-md active:opacity-75 md:hover:scale-110 md:hover:cursor-pointer md:transition-all'>
-      <img src={src} alt={alt} className='w-full rounded-t-md' />
-      <h3 className='p-2 text-xl text-white font-medium selection:bg-white selection:text-neutral-700'>{title}</h3>
+    <div className='md:w-1/5 bg-white dark:bg-neutral-600 rounded-lg active:opacity-75 md:hover:scale-110 md:hover:cursor-pointer md:transition-all'>
+      <img src={src} alt={alt} className='w-full rounded-t-lg' />
+      <h3 className='p-2 text-xl dark:text-neutral-200 font-medium'>{title}</h3>
     </div>
   );
 }
