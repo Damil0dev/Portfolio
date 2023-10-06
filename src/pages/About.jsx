b120e0245938e1def6d71ac02fa8975f0e9fe298
Damian Ysla <ysladamian@outlook.com>
2023-10-06 19:17:56 -0300

    move certificates to skills page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,11 @@
-import certificado1 from "../assets/certificados/certificado1.jpg";
-import certificado2 from "../assets/certificados/certificado2.jpg";
-import certificado3 from "../assets/certificados/certificado3.jpg";
+
 
 function About() {
   return (
-    <div className='w-full flex flex-col justify-center items-center gap-5 py-20 dark:bg-neutral-800'>
+    <div className='w-full h-screen flex flex-col justify-center items-center gap-5 py-20 dark:bg-neutral-800'>
       <div className='max-w-xl flex flex-col gap-5 px-5'>
         <Title />
         <Text />
-      </div>
-      <div className='flex flex-col items-center'>
-        <h2 className='w-fit m-2 text-4xl text-neutral-800 dark:text-neutral-200 border-b-4 border-blue-800 text-center font-semibold'>
-          Certificates
-        </h2>
-        <Certificados />
       </div>
     </div>
   );
@@ -43,35 +35,6 @@ function Text() {
   );
 }
 
-function Certificados() {
-  return (
-    <div className='flex flex-col md:flex-row md:justify-center gap-5 p-5'>
-      <Certificado
-        src={certificado1}
-        alt='Digital Marketing certificate'
-        title='Digital Marketing'
-      />
-      <Certificado
-        src={certificado2}
-        alt='Computer Systems Technician certificate'
-        title='Computer Systems Technician'
-      />
-      <Certificado
-        src={certificado3}
-        alt='META Front-End Developer certificate'
-        title='META Front-End Developer'
-      />
-    </div>
-  );
-}
 
-function Certificado({ src, alt, title }) {
-  return (
-    <div className='md:w-1/5 bg-white dark:bg-neutral-700 rounded-lg active:opacity-75 md:hover:scale-110 md:hover:cursor-pointer md:transition-all'>
-      <img src={src} alt={alt} className='w-full rounded-t-lg' />
-      <h3 className='p-2 text-xl dark:text-neutral-200 font-medium'>{title}</h3>
-    </div>
-  );
-}
 
 export default About;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,78 +6,88 @@ import reactIcon from "../assets/react.svg";
 import tauriIcon from "../assets/tauri.png";
 import vitejsIcon from "../assets/vitejs.svg";
 import pwaIcon from "../assets/pwa.svg";
+import certificado1 from "../assets/certificados/certificado1.jpg";
+import certificado2 from "../assets/certificados/certificado2.jpg";
+import certificado3 from "../assets/certificados/certificado3.jpg";
 
 function Skills() {
   return (
-    <div className='w-full h-screen flex justify-center items-center px-3 '>
-      <div className='w-fit h-3/4 md:h-2/4 px-3 flex flex-row flex-wrap justify-center gap-5 md:gap-8 bg-white dark:bg-neutral-700 rounded-lg shadow-lg'>
-        <SkillBar
-          icon={htmlIcon}
-          alt='excellent level with html'
-          percentaje='80%'
-          firstBarHeight='h-1/6'
-          secondBarHeight='h-5/6'
-        />
-        <SkillBar
-          icon={cssIcon}
-          alt='excellent level with css'
-          percentaje='80%'
-          firstBarHeight='h-1/6'
-          secondBarHeight='h-5/6'
-        />
-        <SkillBar
-          icon={tailwindIcon}
-          alt='excellent level with tailwind'
-          percentaje='80%'
-          firstBarHeight='h-1/6'
-          secondBarHeight='h-5/6'
-        />
-        <SkillBar
-          icon={jsIcon}
-          alt='good level with javascript'
-          percentaje='60%'
-          firstBarHeight='h-2/6'
-          secondBarHeight='h-4/6'
-        />
-        <SkillBar
-          icon={reactIcon}
-          alt='good level with react'
-          percentaje='60%'
-          firstBarHeight='h-2/6'
-          secondBarHeight='h-4/6'
-        />
-        <SkillBar
-          icon={tauriIcon}
-          alt='decent level with tauri'
-          percentaje='40%'
-          firstBarHeight='h-4/6'
-          secondBarHeight='h-2/6'
-        />
-        <SkillBar
-          icon={vitejsIcon}
-          alt='decent level with vite'
-          percentaje='40%'
-          firstBarHeight='h-4/6'
-          secondBarHeight='h-2/6'
-        />
-        <SkillBar
-          icon={pwaIcon}
-          alt='decent level with PWAs'
-          percentaje='40%'
-          firstBarHeight='h-4/6'
-          secondBarHeight='h-2/6'
-        />
-      </div>
+    <div className='w-full h-fit flex flex-col items-center gap-5 py-20 px-5 dark:bg-neutral-800'>
+      <h2 className='w-fit text-4xl text-neutral-800 dark:text-neutral-200 border-b-4 border-blue-800 text-center font-semibold'>
+        Technologies
+      </h2>
+
+      <SkillsSection />
+
+      <h2 className='w-fit text-4xl text-neutral-800 dark:text-neutral-200 border-b-4 border-blue-800 text-center font-semibold'>
+        Certificates
+      </h2>
+
+      <Certificados />
     </div>
   );
 }
 
-function Title() {
+function SkillsSection() {
   return (
-    <p className='text-2xl'>
-      I mainly make websites, but I also have experience in mobile apps, desktop
-      apps and PWAs
-    </p>
+    <div className='min-h-screen md:h-96 md:min-h-0 px-3 flex flex-row flex-wrap justify-center gap-5 md:gap-8 bg-white dark:bg-neutral-700 rounded-lg shadow-lg'>
+      <SkillBar
+        icon={htmlIcon}
+        alt='excellent level with html'
+        percentaje='80%'
+        firstBarHeight='h-1/6'
+        secondBarHeight='h-5/6'
+      />
+      <SkillBar
+        icon={cssIcon}
+        alt='excellent level with css'
+        percentaje='80%'
+        firstBarHeight='h-1/6'
+        secondBarHeight='h-5/6'
+      />
+      <SkillBar
+        icon={tailwindIcon}
+        alt='excellent level with tailwind'
+        percentaje='80%'
+        firstBarHeight='h-1/6'
+        secondBarHeight='h-5/6'
+      />
+      <SkillBar
+        icon={jsIcon}
+        alt='good level with javascript'
+        percentaje='60%'
+        firstBarHeight='h-2/6'
+        secondBarHeight='h-4/6'
+      />
+      <SkillBar
+        icon={reactIcon}
+        alt='good level with react'
+        percentaje='60%'
+        firstBarHeight='h-2/6'
+        secondBarHeight='h-4/6'
+      />
+      <SkillBar
+        icon={tauriIcon}
+        alt='decent level with tauri'
+        percentaje='40%'
+        firstBarHeight='h-4/6'
+        secondBarHeight='h-2/6'
+      />
+      <SkillBar
+        icon={vitejsIcon}
+        alt='decent level with vite'
+        percentaje='40%'
+        firstBarHeight='h-4/6'
+        secondBarHeight='h-2/6'
+      />
+      <SkillBar
+        icon={pwaIcon}
+        alt='decent level with PWAs'
+        percentaje='40%'
+        firstBarHeight='h-4/6'
+        secondBarHeight='h-2/6'
+      />
+    </div>
   );
 }
 
@@ -92,6 +102,37 @@ function SkillBar({ icon, alt, percentaje, firstBarHeight, secondBarHeight }) {
         <div className={`${secondBarHeight} bg-blue-700 rounded-b-xl`}></div>
       </div>
       <img src={icon} alt={alt} width={50} height={50} className='p-2' />
+    </div>
+  );
+}
+
+function Certificados() {
+  return (
+    <div className='flex flex-col md:flex-row md:justify-center gap-5'>
+      <Certificado
+        src={certificado1}
+        alt='Digital Marketing certificate'
+        title='Digital Marketing'
+      />
+      <Certificado
+        src={certificado2}
+        alt='Computer Systems Technician certificate'
+        title='Computer Systems Technician'
+      />
+      <Certificado
+        src={certificado3}
+        alt='META Front-End Developer certificate'
+        title='META Front-End Developer'
+      />
+    </div>
+  );
+}
+
+function Certificado({ src, alt, title }) {
+  return (
+    <div className='md:w-1/5 bg-white dark:bg-neutral-700 rounded-lg active:opacity-75 md:hover:scale-110 md:hover:cursor-pointer md:transition-all'>
+      <img src={src} alt={alt} className='w-full rounded-t-lg' />
+      <h3 className='p-2 text-xl dark:text-neutral-200 font-medium'>{title}</h3>
     </div>
   );
 }
